10c470f2eb001f1798904d2586c91242f03ddc80
Emre YILMAZ <emreyilmaz@yandex.com>
2020-03-28 17:46:55 +0300

    added some tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import SpotifyApi
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "SpotifyApiUtils" $ do
+    it "removes the prefix from string" $ do
+      prefixRemover "device_" "device_id" `shouldBe` ("id":: String)
+    it "generates query string with given list of string, string tuple" $ do
+      (foldl queryString "" [("key","value")] ) `shouldBe` ("&key=value"::String)
+      
